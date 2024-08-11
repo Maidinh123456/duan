28c905d94d448809d85cb1c35af42c57f4af139a
Maidinh123456 <maidinh823532@gmail.com>
2024-08-11 18:06:26 +0700

    Add files via upload

---
+++ demoQ4.py
@@ -0,0 +1,20 @@
+def countWord(fileName,str):
+    # Write your statements here
+    
+    #End your statements
+#end averageNumbers
+#========================DO NOT EDIT GIVEN STATEMENTS IN THE MAIN FUNCTION.============================
+def main():
+  print("TEST Q4 (3 marks):");
+  # fileName = input("Enter file name : ")
+  # str = input("Enter string : ")
+  fileName = 'test\data4.txt'
+  str='he'
+  result = countWord(fileName,str)
+  print("OUTPUT:")
+  print(f"{result}")
+#end main
+#--------------------------------
+if __name__ == "__main__":
+  main()
+#============================================================
